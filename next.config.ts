b05ee2ebc486e06cfc6ed2139c7dfd54fc8d3222
GOAT.S <i.ryu9150@gmail.com>
2024-11-06 17:46:33 +0900

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+  images: {
+    domains: ['yumemi-frontend-engineer-codecheck-api.vercel.app'],
+  },
+  swcMinify: true,
+  outputFileTracing: true,
 };
 
 export default nextConfig;
